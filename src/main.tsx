6920233a364c14e01f8251984ff24157ffb116a9
Kitti <rxsu08560194@gmail.com>
2025-09-09 11:06:49 +0700

    add violet

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "@picocss/pico/css/pico.violet.min.css";
-import "@picocss/pico/css/pico.lime.min.css";
+import "@picocss/pico/css/pico.violet.min.css";
+// import "@picocss/pico/css/pico.lime.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
